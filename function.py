08d1350cb27af3d8cf45ff8f451150d649b4e947
YukeshKumaran <162685072+YukeshKumaran@users.noreply.github.com>
2024-07-19 15:39:42 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,7 +1,13 @@
 def is_leap(year):
-    leap = False
-    if year%4==0:
-        if year%400==0 or year%100==0:
-            leap=true
-    return leap
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
 year = int(input())
+print(is_leap(year))
